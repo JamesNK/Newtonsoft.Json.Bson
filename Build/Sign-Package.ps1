67c30ff8c58b3e70a1d2c72d28c429c8afc651ff
James Newton-King <james@newtonking.com>
2018-10-29 10:57:26 +1300

    Update build to deploy to NuGet

--- Build/Sign-Package.ps1
+++ Build/Sign-Package.ps1
@@ -3,8 +3,8 @@ $currentDirectory = split-path $MyInvocation.MyCommand.Definition
 
 # See if we have the ClientSecret available
 if([string]::IsNullOrEmpty($Env:SignClientSecret)){
-	Write-Host "Client Secret not found, not signing packages"
-	return;
+    Write-Host "Client Secret not found, not signing packages"
+    return;
 }
 
 dotnet tool install --tool-path . SignClient
@@ -13,14 +13,14 @@ dotnet tool install --tool-path . SignClient
 
 $appSettings = "$currentDirectory\appsettings.json"
 
-$nupkgs = gci $Env:ArtifactDirectory\*.nupkg -recurse | Select -ExpandProperty FullName
+$files = gci $Env:ArtifactDirectory\*.nupkg,*.zip -recurse | Select -ExpandProperty FullName
 
-foreach ($nupkg in $nupkgs){
-	Write-Host "Submitting $nupkg for signing"
+foreach ($file in $files){
+    Write-Host "Submitting $file for signing"
 
-	.\SignClient 'sign' -c $appSettings -i $nupkg -r $Env:SignClientUser -s $Env:SignClientSecret -n 'Json.NET BSON' -d 'Json.NET BSON' -u 'https://www.newtonsoft.com/json' 
+    .\SignClient 'sign' -c $appSettings -i $file -r $Env:SignClientUser -s $Env:SignClientSecret -n 'Json.NET BSON' -d 'Json.NET BSON' -u 'https://www.newtonsoft.com/json' 
 
-	Write-Host "Finished signing $nupkg"
+    Write-Host "Finished signing $file"
 }
 
 Write-Host "Sign-package complete"
