eac4ddff9f9a27eeddb1784514a26edfc273983f
James Newton-King <james@newtonking.com>
2025-05-29 13:04:00 +0800

    Migrate from sln to slnx

--- Build/build.ps1
+++ Build/build.ps1
@@ -118,7 +118,7 @@ task Test -depends Build {
 
 function NetCliBuild()
 {
-  $projectPath = "$sourceDir\Newtonsoft.Json.Bson.sln"
+  $projectPath = "$sourceDir\Newtonsoft.Json.Bson.slnx"
   $libraryFrameworks = ($script:enabledBuilds | Select-Object @{Name="Framework";Expression={$_.Framework}} | select -expand Framework) -join ";"
   $testFrameworks = ($script:enabledBuilds | Select-Object @{Name="Resolved";Expression={if ($_.TestFramework -ne $null) { $_.TestFramework } else { $_.Framework }}} | select -expand Resolved) -join ";"
 
